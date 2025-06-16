3df8c438c4fc18bc4a0c99b4effb23e14c45dda0
Nikola Shalamanov <nikolashalamanov999@gmail.com>
2025-06-16 08:19:51 +0300

    added a new Admin Controller so that the adming can ban or delete certain users added the view for it and updated the Layout so that only the Admin can see that view and also translated the Layout in Bulgarian

---
+++ SharedTravelBG/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SharedTravelBG.Models;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+	private readonly UserManager<ApplicationUser> _userManager;
+
+	public AdminController(UserManager<ApplicationUser> userManager)
+	{
+		_userManager = userManager;
+	}
+
+	// GET: /Admin/Users
+	public IActionResult Users()
+	{
+		var users = _userManager.Users.ToList();
+		return View(users);
+	}
+
+	// POST: /Admin/Ban/5
+	[HttpPost]
+	public async Task<IActionResult> Ban(string id)
+	{
+		var user = await _userManager.FindByIdAsync(id);
+		if (user != null)
+		{
+			// Enable lockout and set far-future date
+			await _userManager.SetLockoutEnabledAsync(user, true);
+			await _userManager.SetLockoutEndDateAsync(user, DateTimeOffset.MaxValue);
+		}
+		return RedirectToAction(nameof(Users));
+	}
+
+	// POST: /Admin/Unban/5
+	[HttpPost]
+	public async Task<IActionResult> Unban(string id)
+	{
+		var user = await _userManager.FindByIdAsync(id);
+		if (user != null)
+		{
+			await _userManager.SetLockoutEndDateAsync(user, null);
+		}
+		return RedirectToAction(nameof(Users));
+	}
+
+	// POST: /Admin/Delete/5
+	[HttpPost]
+	public async Task<IActionResult> Delete(string id)
+	{
+		var user = await _userManager.FindByIdAsync(id);
+		if (user != null)
+		{
+			await _userManager.DeleteAsync(user);
+		}
+		return RedirectToAction(nameof(Users));
+	}
+}
